5b9d2fa098c37f803e57a0f774c702eccdf2633b
amit28manchanda <amit28manchanda@mail.com>
2017-08-30 12:45:42 +0530

    image gem installation with minor changes at required places

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,9 @@
 class Product < ApplicationRecord
      CATEGORY = %w[mobile laptop tablet].freeze
 
-
-
+has_many :reviews
+validates_uniqueness_of :name
 validates_presence_of :name ,:price, :category
+
+mount_uploader :image ,ImageUploader
 end
